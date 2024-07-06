a32e8ce9587c586bbe4bfdff68b3f49f40aaffc2
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2024-07-06 17:22:14 -0300

    feat: start a new project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from "react-native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontRobotoIsLoaded] = useFonts({
+    'robotoRegular': Roboto_400Regular,
+    'robotoBold': Roboto_700Bold
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: '#202024',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontRobotoIsLoaded && <Text style={{ color: '#FFFFFF', fontFamily: 'robotoRegular' }}>Hello world!</Text>}
+      {!fontRobotoIsLoaded && <Text>Font loading!</Text>}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
